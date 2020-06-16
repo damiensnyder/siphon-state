0c96c4b665f32620bc4cf1f9502493f68d3b53d8
Damien Snyder <damiensnyder@damiensnyder.com>
2020-06-16 15:00:28 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import io from 'socket.io-client';
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "unchanged"
-    };
-  }
-
-  componentDidMount() {
-    this.socket = io();
-    this.socket.on('connected', (data) => {
-      this.setState({
-        text: data.msg
-      });
-    });
-  }
-
-  render() {
-    return <div>{this.state.text}</div>;
-  }
+function Home() {
+  return <div>Click <a href={'/game/oaxm'}>here</a> to play a game.</div>;
 }
 
 export default Home;
