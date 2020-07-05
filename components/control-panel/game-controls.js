aba250df6326b256c285da580b9075d7169d9eec
Damien Snyder <damiensnyder@damiensnyder.com>
2020-07-05 11:06:11 -0700

    make GameControls a functional component

--- components/control-panel/game-controls.js
+++ components/control-panel/game-controls.js
@@ -1,53 +1,45 @@
 import React from 'react';
 
 import PolCategory from '../pol-category';
-
 import styles from './game-controls.module.css';
 
-class GameControls extends React.Component {
-  constructor(props) {
-    super(props);
+function GameControls() {
+  const gs = this.props.gs;
+  const self = gs.parties[gs.pov];
+  const runnable = [];
+  const symps = [];
+
+  for (let i = 0; i < self.pols.length; i++) {
+    let pol = self.pols[i];
+    if (gs.pols[pol].runnable) {
+      runnable.push(pol);
+    }
   }
 
-  // TODO: actually write this shit, cmon
-  render() {
-    const gs = this.props.gs;
-    const self = gs.parties[gs.pov];
-    const runnable = [];
-    const symps = [];
+  for (let i = 0; i < self.symps.length; i++) {
+    symps.push(self.symps[i]);
+  }
 
-    for (let i = 0; i < self.pols.length; i++) {
-      let pol = self.pols[i];
-      if (gs.pols[pol].runnable) {
-        runnable.push(pol);
-      }
-    }
-
-    for (let i = 0; i < self.symps.length; i++) {
-      symps.push(self.symps[i]);
-    }
-
-    return (
-      <div className={styles.outerWrapper}>
-        <div className={styles.innerWrapper}>
-          <div>
-            <PolCategory gs={gs}
-                         callback={this.props.callback}
-                         type={"Available"}
-                         inProvince={false}
-                         pols={runnable} />
-          </div>
-          <div>
-            <PolCategory gs={gs}
-                         callback={this.props.callback}
-                         type={"Sympathizers"}
-                         inProvince={false}
-                         pols={symps} />
-          </div>
+  return (
+    <div className={styles.outerWrapper}>
+      <div className={styles.innerWrapper}>
+        <div>
+          <PolCategory gs={gs}
+                       callback={this.props.callback}
+                       type={"Available"}
+                       inProvince={false}
+                       pols={runnable} />
+        </div>
+        <div>
+          <PolCategory gs={gs}
+                       callback={this.props.callback}
+                       type={"Sympathizers"}
+                       inProvince={false}
+                       pols={symps} />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default GameControls;
