91d870069566d3102a4377d466bd429135788a21
Damien Snyder <damiensnyder@damiensnyder.com>
2020-07-10 21:49:10 -0700

    added invite link to spectator

--- components/game/control-panel/panel-switcher.js
+++ components/game/control-panel/panel-switcher.js
@@ -3,30 +3,32 @@ import React from 'react';
 import JoinPanel from './join-panel';
 import WaitingPanel from './waiting-panel';
 import GameControls from './game-controls';
+import InviteLink from './invite-link';
+import general from '../../general.module.css';
 
 function ControlPanel(props) {
   if (props.gs.pov < 0 && !props.gs.started) {
     return (
       <JoinPanel callback={props.callback}
-                 gameCode={props.gameCode} />
-    );
-  } else if (props.gs.pov >= 0 && props.gs.ended) {
-    return (
-      <div>
-        {props.gs.parties[props.gs.winner].name} wins
-        <button onClick={e => props.callback('ready', {})}>
-          Rematch
-        </button>
-      </div>
+        gameCode={props.gameCode} />
     );
   } else if (props.gs.pov >= 0 && !props.gs.started) {
-    return <WaitingPanel gs={props.gs}
-                         callback={props.callback} />;
+    return (
+      <WaitingPanel gs={props.gs}
+          gameCode={props.gameCode}
+          callback={props.callback} />
+    );
   } else if (props.gs.pov >= 0 && props.gs.started) {
-    return <GameControls gs={props.gs}
-                         callback={props.callback} />;
+    return (
+      <GameControls gs={props.gs}
+          callback={props.callback} />
+    );
   } else {
-    return <div>enjoy the show</div>;
+    return (
+      <div className={general.outerWrapper + ' ' + general.horizWrapper}>
+        <InviteLink gameCode={props.gameCode} />
+      </div>
+    );
   }
 }
 
