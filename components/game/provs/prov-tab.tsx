2917dcf0bce6ff13bfea591fa4154600f3f6c3ba
Damien Snyder <damiensnyder@damiensnyder.com>
2020-08-26 15:44:37 -0700

    minor fix to provs header

--- components/game/provs/prov-tab.tsx
+++ components/game/provs/prov-tab.tsx
@@ -5,7 +5,6 @@ import styles from "./provs-header.module.css";
 function ProvTab(props) {
   return (
     <div className={styles.tabOuter + ' ' +
-        (/*props.active ? styles.activeTab :*/ styles.inactiveTab) + ' ' +
         (props.open ? styles.openTab : styles.closedTab)}
         onClick={() => props.tabCallback(props.index)}>
       {props.name}
--- components/game/provs/provs-header.tsx
+++ components/game/provs/provs-header.tsx
@@ -3,17 +3,18 @@ import React from "react";
 import ProvTab from "./prov-tab";
 import styles from "./provs-header.module.css";
 
-function tabsToJsx(props) {
-  const tabsJsx = [];
+function tabsToJsx(props): React.ReactNode {
+  const tabsJsx: React.ReactNode[] = [];
   props.gs.provs.forEach((prov, provIndex) => {
     tabsJsx.push(
-      <ProvTab name={props.gs.provs[provIndex].name}
+      <ProvTab name={props.gs.provs[provIndex].name +
+              (prov.seats > 1 ? ` (${prov.seats})` : "")}
           index={provIndex}
           key={provIndex}
           open={props.activeTab == provIndex}
           tabCallback={props.tabCallback} />
     );
-    if (provIndex < 4) {
+    if (provIndex < props.gs.provs.length - 1) {
       tabsJsx.push(
         <div className={styles.pointer}
             key={provIndex + 5}>
